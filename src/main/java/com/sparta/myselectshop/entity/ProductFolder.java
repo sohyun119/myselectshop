3590c3dc09650d9e4cba020e602fc2d03c3ad0a6
Sohyun <sh-119@naver.com>
2025-02-06 03:16:44 +0900

    상품 폴더 설계

---
+++ src/main/java/com/sparta/myselectshop/entity/ProductFolder.java
@@ -0,0 +1,32 @@
+package com.sparta.myselectshop.entity;
+
+import jakarta.persistence.*;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Entity
+@Getter
+@Setter
+@NoArgsConstructor
+@Table(name = "product_folder")
+public class ProductFolder {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    // 외래키
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "product_id", nullable = false)
+    private Product product;
+
+    // 외래키
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "folder_id", nullable = false)
+    private Folder folder;
+
+    public ProductFolder(Product product, Folder folder) {
+        this.product = product;
+        this.folder = folder;
+    }
+}
---
+++ src/main/java/com/sparta/myselectshop/repository/FolderRepository.java
@@ -0,0 +1,9 @@
+package com.sparta.myselectshop.repository;
+
+import com.sparta.myselectshop.entity.Folder;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface FolderRepository extends JpaRepository<Folder, Long> {
+
+
+}
